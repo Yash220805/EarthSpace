487d3d590558a237d3854b3b4fb5da22114be95b
Yash Dubey <yashdubey2285@gmail.com>
2025-07-16 13:36:59 +0530

    Initial commit with separated files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+AOS.init({ duration: 1000, once: true });
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 2 },
+    move: { enable: true, speed: 1 },
+  },
+  interactivity: {
+    events: { onhover: { enable: true, mode: "repulse" } },
+  },
+  retina_detect: true,
+});
